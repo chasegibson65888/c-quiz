505e17fc8543a7046f3b49ed7c18fafae9e3094b
chasegibson65888 <55086314+chasegibson65888@users.noreply.github.com>
2020-06-28 20:50:00 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "what is the text portion of most websites written in?",
+        choices: ["<java>", "<html>", "<script>", "<img>"],
+        answer: "<html>"
+    },
+    {
+        title: "A database language that is most commonly used is?",
+        choices: ["sql", "python", "c#", "C+"],
+        answer: "sql"
+    },
+    {
+        title: "How do you console log a statement?",
+        choices: ["<console.log'>", "<'print'>", "<script'>", "<'write'>"],
+        answer: "<console.log'>"
+    },
+    {
+        title: "Which type of pop up box will allow a user to type a response?",
+        choices: ["input", "prompt", "alert", "confirm"],
+        answer: "prompt"
+    },
+    {
+        title: "What is a DOM in JavaScript?",
+        choices: ["Data of Mine", "Document of Master", "Data Object Modal", "Document Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "Is python or javascript more difficult",
+        choices: ["python", "js", "I have no clue", "neither or"],
+        answer: "neither or"
+    },
+];// JavaScript source code
